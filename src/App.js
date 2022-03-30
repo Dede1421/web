56c769010e436bd6abf17472332e25fc7b443bfd
Dede1421 <mauricioandre91@gmail.com>
2022-03-30 16:16:20 -0500

    style: Se añadió el Banner de la página

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
+import Banner from './Components/Banner';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Banner/>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/Components/Banner.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Polar from "../Images/Polar.jpg"
+
+const Banner = () => { 
+    return (
+        <main>
+            <section className="Banner" style={{ backgroundImage: `url(${Polar})`}}>            
+            </section>
+        </main>
+        
+    )
+}
+
+export default Banner
