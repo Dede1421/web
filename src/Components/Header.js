cd04f8c568a0e384f3825e28f86769c4859a9865
Dede1421 <mauricioandre91@gmail.com>
2022-03-30 15:45:30 -0500

    style: se finalizó el diseño del header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,16 @@
 import React from "react";
-import NatGeoLogo from "../Images/NatGeoLogo.png"
+import NatGeoLogo from "../Images/NatGeoLogo.png";
 import { Grid, Link } from "@mui/material";
+import "../Style.css"
 
 const Header = () => {
     return (
         <header>
-            <Grid container justifyContent="space-between">
+            <Grid container justifyContent="space-between" alignItems="center" style={{padding: "15px"}}>
                 <Link href="#">
                     <img src={NatGeoLogo} alt="" style={{height: "80px"}}></img>
                 </Link>
-                    <ul style={{}}>
+                    <ul className="nav">
                         <li>PROGRAMACIÓN TV</li>
                         <li>CORONAVIRUS</li>
                         <li>FOTO DEL DÍA</li>
