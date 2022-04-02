69a8f23e51c7be62c23f5493d121c56b6d1b03d6
Dede1421 <mauricioandre91@gmail.com>
2022-04-02 16:22:11 -0500

    feat: se añadio modal

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,23 +1,63 @@
-import { Button, Grid } from "@mui/material";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid } from "@mui/material";
 import React from "react";
 import "../Style.css"
 
 const Footer = () => {
+    const [open, setOpen] = React.useState(false)
+
+    const handlerOpen = (event) => {
+        setOpen(true)
+        event.preventDefault()
+    }
+
+    const handlerClose = () => {
+        setOpen(false)
+    }
+
     return (
         <footer style={{ padding: "50px"}}>
             <Grid container justifyContent="space-between">
-               <form>
+               <form onSubmit={handlerOpen}>
                     <div>
                         <h3 style={{ textAlign: "left"}}>MAS INFORMACIÓN</h3>
-                        <input type="email" placeholder="E-MAIL"/>
-                        <button className="btn" type="submit">NEWSLETTER</button>
+                        <input type="email" placeholder="E-MAIL" required/>
+                        <button 
+                            className="btn" 
+                            type="submit"
+                            >
+                                NEWSLETTER
+                        </button>
                     </div>
                 </form>     
                 <div className="social">
                     <h3>SEGUIR</h3>
                 </div>                              
-            </Grid>           
+            </Grid>
+            <NewsletterDialog open={open} handlerClose={handlerClose}/>           
         </footer>
+    )
+}
+
+const NewsletterDialog = ({open, handlerClose}) => {
+    
+    return (
+        <Dialog open={open}>
+            <DialogTitle>
+                Thanks =D
+            </DialogTitle>
+            <DialogContent>
+                An email has been sent with a link to reset your password. Click the link in
+                the email and follow the instructions.
+            </DialogContent>
+            <DialogActions>
+                <Button 
+                    className="btn"
+                    type="submit"
+                    onClick={handlerClose}>
+                        DE ACUERDO
+                </Button>
+            </DialogActions>
+        </Dialog>
     )
 }
 
